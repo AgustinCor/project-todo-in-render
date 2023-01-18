2270755d047d247406a16b99ff87510faaa51f50
AgustinCor <agustin.cordero@gmail.com>
2023-01-18 12:46:15 -0300

    render deploy

---
+++ src/utils/database.js
@@ -0,0 +1,28 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+// crear una instancia con parametros de configuración de nuestra base de datos
+// un objeto de configuración --> credenciales de mi base de datos
+const db = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER, // postgres para ustedes
+  host: process.env.DB_HOST, // 127.0.0.1
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD, // pones tu contraseña
+  dialect: "postgres", // la base de datos que estamos usando
+  logging: false,
+}); 
+
+
+/*const db = new Sequelize({
+  database:"todoapp",
+  username:"postgres",
+  host:"localhost",
+  port:"5432",
+  password:"1234",
+  dialect:"postgres",
+  logging:false,
+});*/
+
+
+module.exports = db;
